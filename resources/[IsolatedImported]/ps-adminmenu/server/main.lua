77d3bd14241df75cd3805e50275285b9e879b7b6
IsolatedRP <isolatedrp@etherxi.com>
2024-01-21 23:47:53 +0000

    IsolatedRP Dev Sevrer
    Co-Authored-By: JohnySilver <45969461+johnysilver@users.noreply.github.com>

--- resources/[IsolatedImported]/ps-adminmenu/server/main.lua
+++
@@ -1,9 +0,0 @@
-QBCore = exports['qb-core']:GetCoreObject()
-
-lib.addCommand('admin', {
-    help = 'Open the admin menu',
-    restricted = 'qbcore.mod'
-}, function(source)
-    TriggerClientEvent('ps-adminmenu:client:OpenUI', source)
-end)
--- Callbacks
--- resources/[IsolatedImported]/ps-multijob/svelte-source/src/tests/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from 'vitest';
-
-describe('function()', async () => {
-  test('behavior', () => {
-    expect(1).toBe(1);
-  });
-});
